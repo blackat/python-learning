98d58891049a151dbcb157c3d29c9ff063b1bd0d
Eugenio Lentini <eugenio_lentini@fastmail.fm>
2026-03-28 12:20:02 +0100

    Add under the hood collapsible block

--- docs-site/.astro/content-modules.mjs
+++ docs-site/.astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/docs/ch01_oop/02_foundation_inheritance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fch01_oop%2F02_foundation_inheritance.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/ch01_oop/02_foundation_inheritance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fch01_oop%2F02_foundation_inheritance.mdx&astroContentModuleFlag=true")],
+["src/content/docs/ch01_oop/properties.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fch01_oop%2Fproperties.mdx&astroContentModuleFlag=true")]]);
 		
